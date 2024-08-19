8404f3b68ecc53a609cdcdae94b7548a7a09fe26
nmoreno <nahuelmoreno.dev@gmail.com>
2024-08-19 01:20:02 -0300

    fix on ready event

--- src/bot/events/onReady.ts
+++ src/bot/events/onReady.ts
@@ -1,9 +1,10 @@
 import { Client, Events } from 'discord.js';
+import { DiscordBot } from '..';
 
 export default {
   name: Events.ClientReady,
   once: true, // Se ejecuta solo una vez
-  execute(client: Client) {
+  execute(bot: DiscordBot, client: Client) {
     console.log(`Ready! Logged in as ${client.user?.tag}`);
   },
 };
